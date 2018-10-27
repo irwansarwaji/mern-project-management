f762f8092a72eed0874582bd0c5d81c0f767e8f4
Irwan Sarwaji <irwan_sarwaji@outlook.com>
2018-10-27 10:52:14 +0200

    Version 1.1

--- client/src/store.js
+++ client/src/store.js
@@ -5,10 +5,28 @@ import rootReducer from './reducers';
 const initialState = {}; //empty object
 const middleware =[thunk]; //any middleware we use that we are using in an array (this case thunk)
 //also takes middleware
-const store = createStore(rootReducer,initialState, compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    window._REDUX_DEVTOOLS_EXTENSION_ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
-));
+
+const composeEnhancers =  typeof window === 'object' &&  window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const composingMiddlewareAndDevTools = composeEnhancers(applyMiddleware(...middleware));
+
+const store = createStore(rootReducer, initialState, composingMiddlewareAndDevTools);
+
+console.log(store.getState());
 
 export default store;
+
+
+
+
+
+
+//
+// const store = createStore(rootReducer,initialState, compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+//     window._REDUX_DEVTOOLS_EXTENSION_ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+// ));
+//
+// export default store;
